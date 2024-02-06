3a2e0d9199b71db62c53329edd6c084b45cabea6
chevonnelise <elise.chevonne@gmail.com>
2024-02-06 08:15:22 +0000

    create map.js

--- 06-foursquare-api/data.js
+++ 06-foursquare-api/data.js
@@ -1,13 +1,17 @@
-async function search () {
+async function search (lat, lng, searchTerms) {
     const response = await axios.get('https://api.foursquare.com/v3/places/search', {
         params: {
-            query:"Chicken RIce",
-            near:"Singapore"
+            query:encodeURI(searchTerms), // encodeURI function to convert special characteres to their encoded 
+            ll: lat + "," + lng,
+            // categories:"13033", //enable use of categories
+            sort:"DISTANCE",
+            radius: 2000,
+            limit: 50
         },
-        headers:(
+        headers:{
             accept:"application/json",
             Authorization: 'fsq3fCgI4pxZ46odFRivTiN+qWXmlzXCWoMRWImvmkCqINs='
-        )
+        }
     })
     console.log(response.data);
 }
---
+++ 06-foursquare-api/map.js
@@ -0,0 +1,6 @@
+function createMap(mapContainerID, lat, lng) {
+    const map = L.map(mapContainerID);
+    map.setView([lat, lng],13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
+    return map;
+}
