19baca40cb7319e9e5f896730e7f265e37192682
chevonnelise <elise.chevonne@gmail.com>
2024-02-06 08:39:12 +0000

    edited script.js

--- 06-foursquare-api/map.js
+++ 06-foursquare-api/map.js
@@ -4,3 +4,4 @@ function createMap(mapContainerID, lat, lng) {
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
     return map;
 }
+
--- 06-foursquare-api/script.js
+++ 06-foursquare-api/script.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+    const map = createMap('map', 1.3521, 103.8198);
+
+    document.addEventListener("DOMContentLoaded", async function () {
+        const searchTerms = document.querySelector("#searchTerms").value;
+        const results = await search(1.3521, 103.8298, searchTerms);
+        console.log(data);
+
+        for (let location of data.results) {
+            const lat = location.geocodes.main.latitude;
+            const lng = location.geocodes.main.longitude;
+            const address = location.location.formatted_address;
+            const marker = L.marker([lat, lng]);
+            marker.bindPopup(`<h1>${address}</h1>`)
+
+
+            marker.addTo(map);
+        }
+    })
 
 });
