da55bfacfa1bf97518b325d095d401291dd50f00
chevonnelise <elise.chevonne@gmail.com>
2024-02-07 03:37:49 +0000

    add search results

--- 06-foursquare-api/map.js
+++ 06-foursquare-api/map.js
@@ -16,7 +16,7 @@ function createMap(mapContainerID, lat, lng) {
 /**
  * Add markers to a map
  * @param {[*]} searchResults Array of objects from FourSquare
- * @param {*} map Leaflet Map Object
+ * @param {*} layer The Leaflet Layer to add to
  */
 function addMarkersToMap(searchResults, layer) {
      // add markers:
@@ -24,15 +24,18 @@ function addMarkersToMap(searchResults, layer) {
         // x.results[0].geocodes.main.latitude = lat
         // x.results[0].goecodes.main.longtitude = lng
 
+        // remove all existing markers from the provided layer
+        layer.clearLayers();
+
         // take one location at a time from data.results
         for (let location of searchResults.results) {
             // create a marker for that location
             const lat = location.geocodes.main.latitude;
             const lng = location.geocodes.main.longitude;
             const address = location.location.formatted_address;
-            const name = location.name
+            const name = location.name;
             const marker = L.marker([lat, lng]);
-            marker.bindPopup(`<h1>${address}</h1>`);
+            marker.bindPopup(`<h1>${name}</h1><h2>${address}</h2>`);
 
             // add the marker to the map
             marker.addTo(layer);
--- 06-foursquare-api/script.js
+++ 06-foursquare-api/script.js
@@ -1,19 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const map = createMap('map', 1.3521, 103.8198);
+    const searchLayer = L.layerGroup();
+    searchLayer.addTo(map);
 
     document.querySelector("#searchBtn").addEventListener("click", async function () {
         const searchTerms = document.querySelector("#searchTerms").value;
         
+        // find the lat lng of the center of the map
         const centerPoint = map.getBounds().getCenter();
-        const searchLayer = L.layerGroup()
-        searchLayer.addTo(map);
-        
-        console.log(centerPoint);
-        
-        const data = await search(centerPoint, 1.3521, 103.8198, searchTerms);
+        const data = await search(centerPoint.lat, centerPoint.lng, searchTerms);
 
-       
-        addMarkersToMap(data, map);
+        
+        addMarkersToMap(data, searchLayer);
 
 
     })
